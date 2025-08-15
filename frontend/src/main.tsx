d40f4220d51824d295cf8e4dfffd56b9f35203fb
najeem <Mohamed.Najeem@cns-me.com>
2025-08-15 09:11:44 +0400

    renamign to caps

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRoutes from "./routes";
-import "./app.css";
+import "./App.css";
 import { ThemeProvider } from "@emotion/react";
 import theme from "./themes";
 
